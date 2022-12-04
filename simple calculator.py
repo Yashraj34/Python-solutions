c0fbc520dd190f42be270fd07dc64b71f8483fbf
Yashraj34 <89093009+Yashraj34@users.noreply.github.com>
2022-12-04 07:53:00 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the number:"))
+num2=int(input("Enter the number:"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("the addition value is ",add)
+print("the subtraction value is",sub)
+print("the multiplication value is",mul)
+print("the division value is",div)
